e785c1bb92391961528e9c27390e85d725f4d4b6
javeed <shaikafridi557@gmail.com>
2025-08-25 21:18:07 +0530

    feat: update db connection to use env variable

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,7 +1,7 @@
 
 import { MongoClient } from 'mongodb';
 
-const url = 'mongodb://localhost:27017';
+const url = process.env.DATABASE_URL || 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
 let db;
